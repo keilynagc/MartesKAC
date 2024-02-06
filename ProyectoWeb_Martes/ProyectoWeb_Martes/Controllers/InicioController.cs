ee21ca4771c3637851d63943aac39a63738651c0
Keilyn Aguero C <90811028+keilynagc@users.noreply.github.com>
2024-02-06 16:33:44 -0600

    Commit inicial

---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -0,0 +1,54 @@
+﻿using ProyectoWeb_Martes.Entidades;
+using System.Web.Mvc;
+
+namespace ProyectoWeb_Martes.Controllers
+{
+    public class InicioController : Controller
+    {
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(Usuario entidad)
+        {
+            return RedirectToAction("PantallaPrincipal", "Inicio");
+        }
+
+
+        [HttpGet]
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarUsuario(Usuario entidad)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario entidad)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult PantallaPrincipal()
+        {
+            return View();
+        }
+
+    }
+}
